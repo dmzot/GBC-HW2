36db06141e59cfd4ec1c2b99a58172df93f17170
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 16:09:38 +0600

    Solution to Task 2 added

---
+++ HW2 - Task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите число 1: ");
+int X1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int X2 = int.Parse(Console.ReadLine());
+
+if (X1 / X2 == X2 || X2 / X1 == X1) Console.WriteLine("Одно число является квадратом другого");
+else if (X1 == 0 || X2 == 0) Console.WriteLine("Введите числа, не равные нулю");
+else Console.WriteLine("Одно число не является квадратом другого");
