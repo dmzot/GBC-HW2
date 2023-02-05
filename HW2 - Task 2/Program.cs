f43c58a02dec6fcc2320edc8c77aa91ca54688d2
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 16:12:16 +0600

    Updated solution to Task 2 added

--- HW2 - Task 2/Program.cs
+++ HW2 - Task 2/Program.cs
@@ -6,6 +6,6 @@ int X1 = int.Parse(Console.ReadLine());
 Console.Write("Введите число 2: ");
 int X2 = int.Parse(Console.ReadLine());
 
-if (X1 / X2 == X2 || X2 / X1 == X1) Console.WriteLine("Одно число является квадратом другого");
-else if (X1 == 0 || X2 == 0) Console.WriteLine("Введите числа, не равные нулю");
-else Console.WriteLine("Одно число не является квадратом другого");
+if (X1 == 0 || X2 == 0) Console.WriteLine("Введите числа, не равные нулю");
+else if (X1 / X2 == X2 || X2 / X1 == X1) Console.WriteLine("Одно число является квадратом другого");
+else Console.WriteLine("Одно число НЕ является квадратом другого");
