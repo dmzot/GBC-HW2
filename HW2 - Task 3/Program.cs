d73ca8da787b913aa4b7f70ab28e9cc795c3997e
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 16:20:17 +0600

    Task 3 solution added

---
+++ HW2 - Task 3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int Z = int.Parse(Console.ReadLine());
+
+if (Z == 1) Console.WriteLine("Диапазон координат при X > 0 и Y > 0");
+else if (Z == 2) Console.WriteLine("Диапазон координат при X < 0 и Y > 0");
+else if (Z == 3) Console.WriteLine("Диапазон координат при X < 0 и Y < 0");
+else if (Z == 4) Console.WriteLine("Диапазон координат при X > 0 и Y < 0");
+else Console.WriteLine("Введите номер четверти от 1 до 4 включительно");
